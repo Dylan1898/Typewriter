cc13959db6148fedf122644b45cf780f93d4dfee
InnovateBham_7 <InnovateBham_7@administartor.local>
2017-06-22 17:08:32 -0500

    inital commit/shiftworking

---
+++ main.js
@@ -0,0 +1,71 @@
+var ids = [33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126]
+$(document).ready(function(){
+    $('#keyboard-upper-container').hide();
+
+// $(document).keypress(function(e){
+//     if(e.which == 16){
+//     // $('keyboard-lower-container').toggle();
+//     // $('keyboard-upper-container').toggle();
+//     console.log( 'shift')
+//     };
+})
+// })
+// $(function() {                           
+//     $(document).keydown(function(e) {
+//         if(e.which == 16){
+//         $('#keyboard-lower-container , #keyboard-upper-container').toggle(function(){
+//         })
+        
+//             console.log('shifted')
+//         }
+//     });
+
+    
+// })
+$(function() {                           
+    $(document).keydown(function(e) {
+        if(e.which == 16){
+        $('#keyboard-lower-container').hide()
+        $('#keyboard-upper-container').show()
+        }   
+    })
+        })
+        $(function() {                           
+    $(document).keyup(function(e) {
+        if(e.which == 16){
+        $('#keyboard-lower-container').show()
+        $('#keyboard-upper-container').hide()
+        }   
+    })
+        })
+        
+
+
+
+
+        $(document).keypress(function(e) {
+               i= e.which  
+               if($.inArray(i, ids)){
+                $("span.well well-lg key").find( "id[i]" ).addClass( ".yellow" )
+                    console.log('it worked')
+                }
+                console.log(e.which)
+        })
+
+
+
+
+
+
+
+
+        // $(document).keypress(function(e) {
+        //         if(e.which == ids[i] ){
+                    
+                    
+        //         $('.well-well-lg-key','id').addClass('.yellow')
+        //             console.log('it worked')
+        //         }
+        //         console.log(e.which)
+        // })
+        
