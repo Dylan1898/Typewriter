49c033c9e686d750e1f67609f24caf5d139b1401
InnovateBham_7 <InnovateBham_7@administartor.local>
2017-06-23 13:04:02 -0500

    sentence keypress recognized

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ var sentences = [
     'oat itain oat tain nate eate tea anne inant nean',
     'itant eate anot eat nato inate eat anot tain eat',
     'nee ene ate ite tent tiet ent ine ene ete ene ate']
-
+counter=0
+var s1 = sentences[0].split('')
+    console.log(s1)
+    console.log(s1[0])
 $(document).ready(function () {
     $('#keyboard-upper-container').hide();
     $(function () {
@@ -30,21 +33,50 @@ $(document).ready(function () {
         var key = $(this).text().trim();
         if (key.length == 1) map[key] = $(this);
     });
-    console.log(map);
+    console.log(map); 
     $(window).keypress(function (e) {
         var key = String.fromCharCode(e.which);
         if (map[key]) map[key].addClass('yellow');
-        
-            console.log('keypress');
-            setTimeout(function () { 
-    $((map[key])).removeClass('yellow');
-}, 200);
-           
+
+        console.log('keypress');
+        setTimeout(function () {
+            $((map[key])).removeClass('yellow');
+        }, 200);
     });
-    $('#sentence').append(sentences[0]);
+
+    
+    // var map = {};
+    // $('span').each(function () {
+    //     var key = $(this).text().trim();
+    //     if (key.length == 1) map[key] = $(this);
+    // });
+    // console.log(map);
+
+    $(window).keypress(function (e) {
+        
+        
+            if (s1[counter]== String.fromCharCode(e.which) ) {
+                counter= counter+1
+                console.log('first')   
+                
+                
+            }
+            
+            else{
+                counter=counter
+            }
+        }
+    )
 
 
 
+
+
+
+        
+    
+});
+$('#sentence').append(sentences[0]);
 
 
 
@@ -70,40 +102,46 @@ $(document).ready(function () {
 
 
 
-        // $(document).keypress(function(e) {
-        //         if(e.which == ids[i] ){
+    // $(document).keypress(function(e) {
+    //         if(e.which == ids[i] ){
 
 
-        //         $('.well-well-lg-key','id').addClass('.yellow')
-        //             console.log('it worked')
-        //         }
-        //         console.log(e.which)
-        // })
+    //         $('.well-well-lg-key','id').addClass('.yellow')
+    //             console.log('it worked')
+    //         }
+    //         console.log(e.which)
+    // })
 
-        // this sorta works
-        // $(document).keypress(function(e) {
-        //         $("span[target='e.which id']")
-        //             console.log('it worked');
-        //             console.log(e.which)
-        //         })
+    // this sorta works
+    // $(document).keypress(function(e) {
+    //         $("span[target='e.which id']")
+    //             console.log('it worked');
+    //             console.log(e.which)
+    //         })
 
-        // $(document).keypress(function(e){
-//     if(e.which == 16){
-//     // $('keyboard-lower-container').toggle();
-//     // $('keyboard-upper-container').toggle();
-//     console.log( 'shift')
-//     };
+    // $(document).keypress(function(e){
+    //     if(e.which == 16){
+    //     // $('keyboard-lower-container').toggle();
+    //     // $('keyboard-upper-container').toggle();
+    //     console.log( 'shift')
+    //     };
 
-// })
-// $(function() {                           
-//     $(document).keydown(function(e) {
-//         if(e.which == 16){
-//         $('#keyboard-lower-container , #keyboard-upper-container').toggle(function(){
-//         })
+    // })
+    // $(function() {                           
+    //     $(document).keydown(function(e) {
+    //         if(e.which == 16){
+    //         $('#keyboard-lower-container , #keyboard-upper-container').toggle(function(){
+    //         })
 
-//             console.log('shifted')
+    //             console.log('shifted')
+    //         }
+    //     });
+// attempt at follow string array
+
+// function progressSentence(e) {
+//         var s1 = sentences[0].split('')
+//         console.log(s1.split(''))
+//         if (String.fromCharCode(e.which) = s1[0]) {
+//             console.log('first')
 //         }
-//     });
-
-
-})
+//     }
