055813c1a042a072c8b8a133c2c83f83ee02c982
InnovateBham_7 <InnovateBham_7@administartor.local>
2017-06-27 11:18:12 -0500

    cleaned up code

--- main.js
+++ main.js
@@ -8,19 +8,6 @@ var letterCounter = 0
 sentenceCounter = 0
 var start = new Date()
 x = sentences[sentenceCounter]
-var sent = [
-    (s1 = sentences[0].split('')),
-    (s2 = sentences[1].split('')),
-    (s3 = sentences[2].split('')),
-    (s4 = sentences[3].split('')),
-    (s5 = sentences[4].split('')),
-]
-console.log(s1)
-console.log(s1[0])
-console.log((sentences[sentenceCounter]))
-
-
-
 $('#keyboard-upper-container').hide();
 $(function () {
     $(document).keydown(function (e) {
@@ -55,24 +42,10 @@ $(window).keypress(function (e) {
         $((map[key])).removeClass('yellow');
     }, 200);
 });
-// var map = {};
-// $('span').each(function () {
-//     var key = $(this).text().trim();
-//     if (key.length == 1) map[key] = $(this);
-// });
-// console.log(map);
-// TRACKING KEYPRESSES
-
 $(document).keypress(function (e) {
-
-    // $('#target-letter').append(x[letterCounter])
     numberOfWords = 54
     numberOfMistakes = 0
-    
     x = sentences[sentenceCounter]
-    // $('#target-letter').append(x[0])
-    // console.log(e.which)
-    // console.log(String.fromCharCode(e.which))
     if (sentenceCounter === sentences.length) {
         var end = new Date()
         var elapsed = end - start
@@ -80,7 +53,6 @@ $(document).keypress(function (e) {
         var minutes = Math.round(seconds / 60);
         $('#feedback').empty()
         $('#feedback').append('Your score is ' + ((numberOfWords / minutes) - (2 * numberOfMistakes)));
-
         setTimeout(function () {
             confirm('Would you like to play again?')
             if (confirm) {
@@ -91,73 +63,37 @@ $(document).keypress(function (e) {
         sentenceCounter = 0
         letterCounter = 0
     }
-    if (letterCounter === x.length) {
-
-        $('#target-letter').append(x[letterCounter - 1])
-        console.log('0000')
-
-    }
     if (letterCounter === x.length - 1) {
-        animateNumber = 17 * letterCounter
-            console.log(animateNumber)
          $('#yellow-block').css('left' , '5.75em')
         letterCounter = 0
         $('#sentence').empty();
         $('#target-letter').empty();
-        console.log('end of sentence');
+        
         sentenceCounter++
-        // $('.row col-xs-12 yellow').removeAttr('style')
-        // setTimeout(function () { $('#yellow-block').animate() }, 10);
-       
         $('#sentence').append(sentences[sentenceCounter])
         x = sentences[sentenceCounter]
         $('#target-letter').append(x[letterCounter])
-
-
     }
-
     $('#feedback').empty()
-
     if (x[letterCounter] == String.fromCharCode(e.which)) {
-          
         $('#target-letter').empty()
         $('#feedback').append('<div class="glyphicon glyphicon-ok"></div>');
         if (letterCounter < x.length) {
             letterCounter = letterCounter + 1
             $('#yellow-block').animate({ left: '+=.75em' }, 12)
-            // console.log('first')
             $('#target-letter').append(x[letterCounter])
-
         }
-
     }
-    //   if (x[letterCounter] == String.fromCharCode(e.which) && (e.which)== 32) {
-
-    //     $('#target-letter').empty()
-    //     $('#feedback').append('<div class="glyphicon glyphicon-ok"></div>');
-    //     if (letterCounter < x.length) {
-    //         letterCounter = letterCounter + 1
-    //         // $('#yellow-block').animate({ left: '+=17px' }, 'fast')
-    //         // console.log('first')
-    //         $('#target-letter').append('x[letterCounter]')
-
-    //     }
-
-    // }
     else if (x[letterCounter] !== String.fromCharCode(e.which)) {
         $('#feedback').append('<div class="glyphicon glyphicon-remove"></div>')
         letterCounter = letterCounter
         // console.log('dosentmatch')
         numberOfMistakes++
-
     }
-
-}
-)
+})
 $(document).ready(function () {
     $('#sentence').append(sentences[sentenceCounter])
     $('#target-letter').append(x[letterCounter])/*MAKE ONLY FIRST LETTER OF FIRST LINE WORK*/
-
 })
 
 
